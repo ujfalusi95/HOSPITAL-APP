df684c2c0dc8a08257837528184af1cbe7e7ff5b
emilio espejo <emilioespejomerino@gmail.com>
2018-12-07 13:26:57 +0100

    Ready to deploy

--- src/main/java/com/emilio/init/JDBCHelper.java
+++ src/main/java/com/emilio/init/JDBCHelper.java
@@ -1,7 +1,8 @@
 package com.emilio.init;
  
 import java.lang.reflect.Method;
-
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -9,33 +10,48 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
+
 
 public class JDBCHelper<T> {
+	private static Connection getConnection() throws URISyntaxException, SQLException {
+	    URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
-	public static Connection getConexion() {
-		Connection conexion = null;
-		Properties properties = new Properties();
-		try {
+	    String username = dbUri.getUserInfo().split(":")[0];
+	    String password = dbUri.getUserInfo().split(":")[1];
+	    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+	    return DriverManager.getConnection(dbUrl, username, password);
+	}
+
+	 //public static Connection getConexion() {
+		 // Connection conexion = null;
+		 //Properties properties = new Properties();
+		 //try {
 			
 			
 			//  2 maneras de hacerla properties.load(JDBCHelper.class.getClassLoader().getResourceAsStream("config.properties"));
-			properties.load(Thread.currentThread().getContextClassLoader()
-					.getResourceAsStream("config.properties"));
-			String driver = System.getenv().get("key.driver");
-			String url = System.getenv().get("key.url");
-			String user = System.getenv().get("key.user");
-			String password = System.getenv().get("key.password");
-			Class.forName(driver);
-			conexion = DriverManager.getConnection(url, user, password);
-		} catch (Exception e) {
-			System.out.println("Error en getConexion: " + e.getMessage());
-		}
-		return conexion;
-	}
+			// properties.load(Thread.currentThread().getContextClassLoader()
+				//	.getResourceAsStream("config.properties"));
+			// String driver = System.getenv().get("key.driver");
+			///String url = System.getenv().get("key.url");
+			//String user = System.getenv().get("key.user");
+			//String password = System.getenv().get("key.password");
+			//Class.forName(driver);
+			//conexion = DriverManager.getConnection(url, user, password);
+		//} catch (Exception e) {
+			//System.out.println("Error en getConexion: " + e.getMessage());
+		//}
+		//return conexion;
+	
+//}
+	
+	
+	
+	
+	
 	@SuppressWarnings({ "deprecation", "unchecked" })
-	public List<T> seleccionarRegistros(String sql, @SuppressWarnings("rawtypes") Class clase) {
-		Connection conexion = getConexion();
+	public List<T> seleccionarRegistros(String sql, @SuppressWarnings("rawtypes") Class clase) throws URISyntaxException, SQLException {
+		Connection conexion = getConnection();
 		Statement sentencia = null;
 		ResultSet filas = null;
 		List<T> listaDeObjetos = new ArrayList<T>();
@@ -102,8 +118,8 @@ public class JDBCHelper<T> {
 	
 	
 	
-	public int modificarRegistro(String sql) {
-		Connection conexion = getConexion();
+	public int modificarRegistro(String sql) throws URISyntaxException, SQLException {
+		Connection conexion = getConnection();
 		Statement sentencia = null;
 		int filasAfectadas = 0;
 		try {

12b2b9ffec2efdd048416cc89f98e3bbe481d4aa
emilio espejo <emilioespejomerino@gmail.com>
2018-12-14 16:32:34 +0100

    ready to deploy

--- src/main/java/com/emilio/classes/Paciente.java
+++ src/main/java/com/emilio/classes/Paciente.java
@@ -304,7 +304,7 @@ public class Paciente {
    }
 		   public  Paciente buscarPacientePorNombre() throws URISyntaxException, SQLException {
 			   JDBCHelper<Paciente> helper = new JDBCHelper<Paciente>();
-				String sql= "SELECT * FROM paciente WHERE nombrepaciente="+this.nombrepaciente;
+				String sql= "SELECT * FROM paciente WHERE nombrepaciente='"+this.dni+"'";
 				List<Paciente> lista = helper.seleccionarRegistros(sql, Paciente.class);
 				return lista.get(0);
    
